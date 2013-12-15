7bdada0aa39b0528c7fbfff6c7c23755c8c9cedc
MatkeziCro <matkezicro@gmail.com>
2013-12-15 16:41:28 +0100

    dodana f-ja ucitajUlaz

--- SemantickiAnalizator.py
+++ SemantickiAnalizator.py
@@ -2,3 +2,33 @@ __author__ = 'Matko'
 
 
 
+
+def ucitajUlaz():
+    ulaz = open("ulaz1.in","r")
+    dict = {}
+
+    for line in ulaz:
+        trenutnaDubina = 0
+        listLine = []
+        listLine.extend(line)
+        del listLine[-1]
+        for item in listLine:
+            if item == ' ':
+                trenutnaDubina += 1
+            else:
+                break
+        stringLine = ''.join(listLine)
+        stringLine.strip()
+
+
+def main ():
+
+    stablo = ucitajUlaz()
+
+
+
+
+
+
+if __name__ == '__main__':
+  main()
