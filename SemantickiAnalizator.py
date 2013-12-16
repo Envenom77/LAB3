4c9d1091215f811741bec644949a003222ae82e3
MatkeziCro <matkezicro@gmail.com>
2013-12-16 22:04:10 +0100

    unaprijeđena metoda nadiDesnuStranu, sada pored elemenata desne strane vraca i indeks gdje se element nalazi(to ej indeks u listaPrograma)

--- SemantickiAnalizator.py
+++ SemantickiAnalizator.py
@@ -18,10 +18,11 @@ class PrijevodnaJedinica(object):
                 break
         return br
 
-
     def nadiDesnuStranu(self,pozicija):
 
         desnaStrana = []
+        element = []
+        indeks = []
 
         brojRazmaka = self.nadiBrojRazmaka(pozicija)
 
@@ -29,11 +30,12 @@ class PrijevodnaJedinica(object):
         noviBrojRazmaka = self.nadiBrojRazmaka(novaPozicija)
 
         while noviBrojRazmaka != brojRazmaka:
-            print novaPozicija
+
             if noviBrojRazmaka == brojRazmaka + 1:
                 tmp = self.listaPrograma[noviBrojRazmaka]
                 tmp = tmp.strip()
-                desnaStrana.append(tmp)
+                element.append(tmp)
+                indeks.append(novaPozicija)
             novaPozicija += 1
 
             #provjera kraja programa, jesmo li doli do kraja liste
@@ -41,10 +43,9 @@ class PrijevodnaJedinica(object):
                 noviBrojRazmaka = self.nadiBrojRazmaka(novaPozicija)
             else:
                 break
+
+        desnaStrana = zip(element,indeks)
         return desnaStrana
-
-
-
 
     def glavnaMetoda(self):
 
@@ -52,10 +53,23 @@ class PrijevodnaJedinica(object):
         #poznavajuci samo indeks u listi programa
         #tako ce se metoda nadiDesnuStranu moci koristit od strane svih objekata koji ju naslijede
 
+        #desna strana dobija listu stvorenu od elemenata od dva clana, prvi je elemtn, a drugi pozicija u listi
         desnaStrana = self.nadiDesnuStranu(self.pozicijaUprogramu)
 
+        if desnaStrana[0][0] == "<vanjska_deklaracija>":
+            pass
+        elif desnaStrana[0][0] == "<prijevodna_jedinica>":
+            pass
+        else:
+            return "ERROR - losa desna strana prijevodne jedinice"
+
+        #ako sve u redu vrati 0
         return 0
 
+class VanjskaDeklaracija(PrijevodnaJedinica):
+
+    def __init__(self,pozicijaUprogramu):
+        self.pozicijaUprogramu = pozicijaUprogramu
 
 
 def ucitajUlaz():
@@ -73,21 +87,15 @@ def main ():
     print listaPrograma
 
     if listaPrograma[0] != "<prijevodna_jedinica>":
-        print "ERROR"
+        print "ERROR - nema prijevodne jedinice"
 
     #stvori inicijalni objekt
     prijevodna_jedinica = PrijevodnaJedinica(listaPrograma,0)
     rezultat = prijevodna_jedinica.glavnaMetoda()
 
-    #je li doslo do pogreske
+    #je li doslo do pogreske, ako je 0 => sve u redu
     if rezultat != 0:
         print rezultat
-
-
-
-
-
-
 
 if __name__ == '__main__':
   main()
