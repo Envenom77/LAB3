c08abaf4ed5c17452468e2b152c4d87ac370b919
MatkeziCro <matkezicro@gmail.com>
2013-12-16 20:30:36 +0100

    uređena f-ja ucitajUlaz

--- SemantickiAnalizator.py
+++ SemantickiAnalizator.py
@@ -5,29 +5,17 @@ __author__ = 'Matko'
 
 def ucitajUlaz():
     ulaz = open("ulaz1.in","r")
-    stablo = {}
+    listaPrograma = ulaz.readlines()
 
-    for line in ulaz:
-        trenutnaDubina = 0
-        listLine = []
-        listLine.extend(line)
-        del listLine[-1]
-        for item in listLine:
-            if item == ' ':
-                trenutnaDubina += 1
-            else:
-                break
-        stringLine = ''.join(listLine)
-        stringLine = stringLine.lstrip()
-
-
-    return stablo
-
+    #makni LF
+    for i in range(len(listaPrograma)):
+        listaPrograma[i] = listaPrograma[i].rstrip()
+    return listaPrograma
 
 def main ():
 
-    stablo = ucitajUlaz()
-    print stablo
+    listaPrograma = ucitajUlaz()
+    print listaPrograma
 
 
 
